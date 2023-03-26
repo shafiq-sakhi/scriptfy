b6775eb7e410f8c18e22efa4196dfb0f23082dc3
bilo1 <karim.tariq@outlook.com>
2023-03-27 01:28:45 +0200

    App texts updated.

--- lib/utils/app_language.dart
+++ lib/utils/app_language.dart
@@ -5,10 +5,10 @@ class AppLanguage {
 
   static get STARS=>isEnglish?"Stars":"Sterne";
 
-  static get FREE_CREDIT_FINSHED=>isEnglish?"Free credit finished":"Gratisguthaben beendet";
+  static get FREE_CREDIT_FINSHED=>isEnglish?"Here you can buy more stars.":"Hier kannst du weitere Sterne kaufen.";
 
   static get PURCHASE_DESCRIPTION => isEnglish
-      ? "You can search and download 12 images in just.Buy and enjoy"
+      ? "You can search and download 12 images in just. Buy and enjoy"
       : "Sie können 12 Bilder in Just suchen und herunterladen. Kaufen und genießen";
 
   static get CLICK_TO_BUY =>
@@ -16,15 +16,15 @@ class AppLanguage {
 
   static get NOT_NOW => isEnglish ? "Not now" : "Nicht jetzt";
 
-  static get TEXT_COPIED => isEnglish ? "Text copied" : "Texte kopiert";
+  static get TEXT_COPIED => isEnglish ? "Text copied" : "Text kopiert";
 
   static get VERIICATION_EMAIL_SENT_PLEASE_VERIFY_EMAIL => isEnglish
-      ? "A verification email was sent. Please verify your email"
-      : "Eine Bestätigungs-E-Mail wurde gesendet. Bitte bestätigen Sie Ihre E-Mail";
+      ? "A verification email from Scriptfy has been sent. Please confirm the verification email"
+      : "Eine Bestätigungs von Scriptfy wurde gesendet. Bitte bestätige die Verfifizierungs E-Mail";
 
   static get ONLINE => isEnglish ? "Online" : "Online";
 
-  static get MENU => isEnglish ? "Menu" : "Speisekarte";
+  static get MENU => isEnglish ? "Menu" : "Menü";
 
   static get CONFIRM_PASSWORD =>
       isEnglish ? "Confirm password" : "Bestätige das Passwort";
@@ -32,8 +32,8 @@ class AppLanguage {
   static get FULNAME => isEnglish ? "FirstName" : "Vorname";
 
   static get GET_STARTED_WITH_SCRIPTFY => isEnglish
-      ? "Get started with ScriptArtify"
-      : "Beginnen Sie mit ScriptArtify";
+      ? "Get started with Scriptfy"
+      : "Beginnene mit Scriptfy";
 
   static get  PASSWORD=>isEnglish?"Password":"Passwort";
 
@@ -43,13 +43,13 @@ class AppLanguage {
 
   static get START_PURCHASED=>isEnglish?"Stars Purchased":"Sterne gekauft";
 
-  static get PURCHASE_STAR=>isEnglish?"Purchase Star":"Stern kaufen";
+  static get PURCHASE_STAR=>isEnglish?"Purchase Star":"Sterne kaufen";
 
-  static get YOUR_ACCOUNT_UPDATED_SUCCESSFULLY=>isEnglish?'Your account updated successfully!':"Ihr Konto wurde erfolgreich aktualisiert!";
+  static get YOUR_ACCOUNT_UPDATED_SUCCESSFULLY=>isEnglish?'Your account updated successfully!':"Dein Konto wurde erfolgreich aktualisiert!";
 
   static get ACCOUNT_CREATED_SUCCESSFULLY => isEnglish
-      ? "Account created successfully.Please check your email"
-      : "Konto erfolgreich erstellt. Bitte überprüfen Sie Ihre E-Mail";
+      ? "Account created successfully. Please confirm the email we have sent."
+      : "Konto erfolgreich erstellt. Bitte Bestätige die E-Mail, die wir gesendet haben.";
 
   static get PHONE_NUMBER => isEnglish ? "Phone number" : "Telefonnummer";
 
@@ -61,8 +61,8 @@ class AppLanguage {
       isEnglish ? "Download completed" : "Download abgeschlossen";
 
   static get YOU_REALLYWANT_TO_DOWNLOAD_ALL_IMAGES => isEnglish
-      ? "You really want to download all images?"
-      : "Sie möchten wirklich alle Bilder herunterladen?";
+      ? "Download all images"
+      : "Alle Bilder herunterladen";
 
   static get STORAGE => isEnglish ? "Storage" : "Lagerung";
 
@@ -78,48 +78,48 @@ class AppLanguage {
       ? "Describe the picture as best as possible."
       : 'Das Bild so gut als möglich beschreiben.';
 
-  static get SCRIPTARTIFY => isEnglish ? 'ScriptArtify' : "ScriptArtify";
+  static get SCRIPTARTIFY => isEnglish ? 'Scriptfy' : "Scriptfy";
 
   static get OPEN_AI_WAITINGFOR_YOUR_QUERY => isEnglish
-      ? "Open AI Waiting for your query..."
-      : "KI öffnen Warten auf Ihre Anfrage...";
+      ? "Scriptfy is looking forward to your chat"
+      : "Scriptfy freut sich auf dein Chat";
 
-  static get CHAT_GPT => isEnglish ? 'Chat GPT Text' : "Chat GPT tekst";
+  static get CHAT_GPT => isEnglish ? 'Scriptfy Code develop' : "Scriptfy Code entwickeln";
 
   static get ENTER_YOUR_PASSWORD_BEFORE_DELETING_ACCOUNT => isEnglish
       ? "Enter your password before deleting account?"
-      : "Geben Sie Ihr Passwort ein, bevor Sie das Konto löschen?";
+      : "Geben dein Passwort ein, bevor du dein Konto löscht?";
 
   static get DELETE_ACCOUNT => isEnglish ? "Delete account" : "Konto löschen";
 
   static String get WELCOME_TO_SCRIPTFY => isEnglish
-      ? "Welcome to ScriptArtify!"
-      : "Herzlich Willkommen bei ScriptArtify!";
+      ? "Welcome to Scriptfy!"
+      : "Herzlich Willkommen bei Scriptfy!";
 
   static String get ABOUT_APP => isEnglish
-      ? "Our app allows you to convert text to images and then share them with friends and family"
-      : "Unsere App ermöglicht es Ihnen, Text in Bilder umzuwandeln und diese dann mit Freunden und Familie zu teilen";
+      ? "Visions can be brought to life through images"
+      : "Visionen können durch Bilder zum Leben erweckt werden";
 
   static String get LETS_GO => isEnglish ? "Here we go!" : "Los geht\'s!";
 
   static String get WELCOME_DESCRITION => isEnglish
-      ? "Featuring ScriptArtify, the app based on Dall-E 2 that converts text into stunning images."
-      : "Mit ScriptArtify, der  App, die auf Dall-E 2 basiert und Text in atemberaubende Bilder umwandelt.";
+      ? "Dall-E2 Scriptfy, the first app based on artificial intelligence that turns text into stunning images. \nIt can also write codes and stories."
+      : "Dall-E2 Scriptfy, der ersten App, die auf künstliche Intelligenz basiert und Texte in atemberaubende Bilder verwandelt. \nDazu noch Codes & Geschichten schreiben kann.";
 
   static String get ENTER_YOUR_EMAIL =>
-      isEnglish ? 'Enter your email' : "Geben sie ihre E-Mail Adresse ein";
+      isEnglish ? 'Enter your email' : "E-Mail Adresse eingeben";
 
   static String get ENTER_YOU_PASSWORD =>
-      isEnglish ? 'Enter your password' : "Geben Sie Ihr Passwort ein";
+      isEnglish ? 'Enter your password' : "Passwort eingeben";
 
   static String get DONT_HAVE_ACCOUNT =>
-      isEnglish ? "don't have account?" : "Sie haben kein Konto?";
+      isEnglish ? "don't have account?" : "Du haben kein Konto?";
 
   static String get SIGNIN => isEnglish ? "Signin" : "Anmelden";
 
   static String get LOADING => isEnglish ? "Loading..." : "Wird geladen...";
 
-  static String get ERROR => isEnglish ? "Error" : "Fehler";
+  static String get ERROR => isEnglish ? "Unfortunately, you've run out of stars." : "Leider sind dir die Sterne ausgegangen.";
 
   static String get NAME_IS_EMPTY =>
       isEnglish ? 'Name field is empty' : "Namensfeld ist leer";
@@ -143,57 +143,57 @@ class AppLanguage {
   static String get REGISTER => isEnglish ? 'Register' : "Registrieren";
 
   static String get ALREAD_HAVE_ACCOUNT =>
-      isEnglish ? "Don't have an account" : "Don't have an account";
+      isEnglish ? "Don't have an account" : "Du hast kein Konto";
 
   static String get ENTER_YOUR_NAME =>
       isEnglish ? 'Enter your Name' : "Gib deinen Namen ein";
 
   static String get ENTER_YOUR_PHONE =>
-      isEnglish ? 'Enter your Phone' : "Geben Sie Ihr Telefon ein";
+      isEnglish ? 'Enter your Phone' : "Telefon eingeben";
 
   static String get ENTER_OUR_CONFIRM_PASSWORD => isEnglish
       ? 'Enter your confirm password'
-      : "Geben Sie Ihr Bestätigungspasswort ein";
+      : "Passwort bestätigen";
 
   static String get SELECT_YOUR_IMAGE =>
-      isEnglish ? 'Select image profile' : "Bildprofil auswählen";
+      isEnglish ? 'Select image profile' : "Profilbild auswählen";
 
   static String get HI => isEnglish ? "Hi" : "Hi";
 
   static String get WHAT_YOU_WANT_TO_DO => isEnglish
       ? 'What do you want to do today?'
-      : "Was willst Du heute machen?";
+      : "Was möchtest du heute machen?";
 
   static String get LOGOUT => isEnglish ? "Logout" : "Ausloggen";
 
   static String get DO_YOU_REALLY_WANT_TO_LOGOUT => isEnglish
       ? "Do you really want to logout?"
-      : "Möchten Sie sich wirklich abmelden?";
+      : "Möchtest du dich wirklich abmelden?";
 
   static String get DO_YOU_REALLY_WANT_TO_DELETE => isEnglish
       ? "Do you really want to delete?"
-      : "Möchten Sie wirklich löschen?";
+      : "Account wirklich löschen?";
 
   static String get YES => isEnglish ? "Yes" : "Ja";
 
   static String get NO => isEnglish ? "No" : "Nein";
 
   static String get TEXT_COMPLETION =>
-      isEnglish ? "Text completion" : "Textvervollständigung";
+      isEnglish ? "Text & Stories" : "Text & Geschichten";
 
   static String get IMAGE_GENERATION =>
-      isEnglish ? "Image generation" : "Bilderzeugung";
+      isEnglish ? "Convert text to images" : "Text in Bilder umwandeln";
 
   static String get CODE_COMPLETION =>
-      isEnglish ? "Code completion" : "Code-Vervollständigung";
+      isEnglish ? "Web & App" : "Web & App";
 
-  static String get EMBEDDING => isEnglish ? "Embeddings" : "Einbettungen";
+  static String get EMBEDDING => isEnglish ? "Coming soon" : "Demnächst verfügbar";
 
   static String get COMMING_SOON => isEnglish ? "Coming soon!" : "Kommt bald!";
 
-  static String get HOME => isEnglish ? 'Home' : "Heim";
+  static String get HOME => isEnglish ? 'Home' : "Home";
 
-  static String get INFO => isEnglish ? 'Info' : "Die Info";
+  static String get INFO => isEnglish ? 'Profile' : "Profil";
 
   static String get PROFILE => isEnglish ? 'Profile' : "Profil";
 
@@ -212,7 +212,7 @@ class AppLanguage {
   static String get UPDATE_LANGUAGE =>
       isEnglish ? "Update language" : "Sprache aktualisieren";
 
-  static String get SUPPORT => isEnglish ? "Support" : "Die Unterstützung";
+  static String get SUPPORT => isEnglish ? "Support" : "Support";
 
   static String get SELECT_LANGUAGE =>
       isEnglish ? "Select Language" : "Sprache auswählen";
@@ -231,11 +231,11 @@ class AppLanguage {
 
   static String get  GALLERY => isEnglish ? "Gallery" : "Galerie";
 
-  static String get  PRIVACY_POLICY => isEnglish ? "Privacy policy" : "Datenschutz-Bestimmungen";
+  static String get  PRIVACY_POLICY => isEnglish ? "Privacy policy" : "Datenschutz";
 
-  static String get  TERM_OF_USE => isEnglish ? "Term of use" : "Nutzungsbedinungen";
+  static String get  TERM_OF_USE => isEnglish ? "Term of use" : "Nutzungs-\nbedinung";
 
-  static String get  CODE_COMPLETION_CHATS => isEnglish ? "Code completion chats" : "Code Vervollständigungs Chats";
+  static String get  CODE_COMPLETION_CHATS => isEnglish ? "Develop something creative" : "Etwas kreatives entwickeln";
 
   static String get  DELETE => isEnglish ? "deleted" : "gelöscht";
 
@@ -247,8 +247,8 @@ class AppLanguage {
 
   static String get  ADD => isEnglish ? "add" : "hinzufügen";
 
-  static String get  DOWNLOADING => isEnglish ? "DOWNLOADING....." : "WIRD HERUNTERGELADEN.....";
+  static String get  DOWNLOADING => isEnglish ? "Image has been downloaded..." : "Bild wurde heruntergeladen...";
 
-  static String get  THE_EMAIL_OR_PASSWORD_IS_WRONG => isEnglish ? "The email or password is wrong!" : "Het e-mailadres of wachtwoord is verkeerd!";
+  static String get  THE_EMAIL_OR_PASSWORD_IS_WRONG => isEnglish ? "The email is wrong!" : "Die E-Mail ist falsch!";
 }
 
